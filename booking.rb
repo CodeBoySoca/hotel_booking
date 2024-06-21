a6c8a4ad31036154a51d28139b968fa1e6ddea98
Rocky Latchman <kidmongoose@gmail.com>
2024-06-21 02:15:23 -0700

    Added classes and methods needed for the project

---
+++ booking.rb
@@ -0,0 +1,18 @@
+
+class Booking
+    #check for your reservation
+   def find_reservation()
+   end
+   
+   #cancel your reservation
+   def cancel_reservation()
+   end
+   
+   #make a reservation
+   def make_reservation()
+   end 
+
+   #Generate invoice for reservation
+   def invoice()
+   end 
+end
---
+++ hotel.rb
@@ -0,0 +1,21 @@
+require 'sqlite3'
+require 'securerandom'
+
+class Hotel
+    #check for all available rooms 
+   def check_availability
+   end 
+   
+   #check for preferred hotel suite
+   def check_suite(suite)
+   end 
+   
+   #check that the desired suite is available at the apprioriate date
+   def check_booking_date(suite, booking_date)
+   end 
+
+   #get hotel suite details(number of beds, floor etc)
+   def get_details
+   end
+   
+end
